748cc587b331a9b3618a037080e3188affd45726
tschaef <tschaef1@gwdg.de>
2015-03-31 13:17:51 +0200

    Add demo letter and page height matching

--- www/js/theme.js
+++ www/js/theme.js
@@ -19,4 +19,53 @@ $(function() {
 		}
 	)
 
+	$('.inserted').hover(
+		function() {
+			$(this).find('.popup').addClass('js-show')
+		},
+		function() {
+			$(this).find('.popup').removeClass('js-show')
+		}
+	)
+
+
+	$('.rotate').each( function() {
+		var min = Math.min( $(this).height(), $(this).width() )
+		var max = Math.max( $(this).height(), $(this).width() )
+		$(this).css({ padding: ((max - min) / 2) + 'px 0' })
+	})
+
+	$('.rotate').click( function() {
+		$(this)
+			.toggleClass('no-margin')
+			.children()
+				.toggleClass('rotate-left')
+	})
+
+	$('.inserted').append('<div class="popup">Einfügung Gerardy</div>')
+
+	var pageHeightsAreMatched = false
+	$('.nav__button--parallel').click( function() {
+		if (pageHeightsAreMatched) {
+			resetPageHeights()
+		} else {
+			matchPageHeights()
+		}
+		pageHeightsAreMatched = !pageHeightsAreMatched
+		$(this).toggleClass('nav__button--highlight')
+	})
+
 })
+
+function matchPageHeights() {
+	$('.viewer--print .page').each( function(index) {
+		var $printPage = $(this)
+		var $transcriptPage = $('.viewer--transcript').find('.page').eq(index)
+		var height = Math.max( $printPage.height(), $transcriptPage.height() )
+		$printPage.add($transcriptPage).css({minHeight: height})
+	})
+}
+
+function resetPageHeights() {
+	$('.viewer .page').css({minHeight: 0})
+}
